b59a069eca1b652e25989e692071400a95e4821c
sunidhitandel <sdt9243@nyu.edu>
2025-03-13 04:44:50 +0000

    adding few changes

--- core/test.py
+++ core/test.py
@@ -4,8 +4,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from core.dataset import get_test_data_loader
-from LilResNets.core.models.MyResNet import get_model
-from core.utils import get_device
+from core.utils import get_device, get_model
 
 def get_device():
     """Get the best available device (CUDA > MPS > CPU)"""
@@ -18,16 +17,53 @@ def get_device():
     return device
 
 
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        data_dict = pickle.load(fo, encoding='bytes')
+    return data_dict
+def get_transforms(normalization=True):
+    test_transform_list = []
+    test_transform_list.append(transforms.ToPILImage())
+    test_transform_list.append(transforms.ToTensor())
+    if normalization:
+        norm_transform = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        test_transform_list.append(norm_transform)
+    test_transforms = transforms.Compose(test_transform_list)
+    return test_transforms
+
+class CIFARTestDataset(Dataset):
+    def __init__(self, data, ids, transform=None):
+        """
+        data: numpy array of shape (N, 32, 32, 3)
+        ids: array-like of test image IDs
+        transform: transforms to apply
+        """
+        self.data = data
+        self.ids = ids
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # If data is stored as (32,32,3)
+        img = self.data[idx].astype("uint8")
+        img_id = self.ids[idx]
+        if self.transform:
+            img = self.transform(img)
+        return img, img_id
+
 def test(config, model_path, output_dir, logger=None):
     """Generate predictions for unlabeled test data"""
     device = get_device()
-
-    # Create output directory if it doesn't exist
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    
-    test_loader = get_test_data_loader(config)
-    model = get_model(config)
+    test_file = "./data/cifar_test_nolabel.pkl"
+    test_dict = unpickle(test_file)
+    test_images = test_dict[b'data'] 
+    test_ids = test_dict[b'ids']
+    transform_test = get_transforms()
+    test_dataset = CIFARTestDataset(test_images, test_ids, transform=transform_test)
+    test_loader = DataLoader(test_dataset, batch_size=int(256/4), shuffle=False, num_workers=16)
+    model,_ = get_model(config)
     model = model.to(device)
     checkpoint = torch.load(model_path, map_location=device)
     if 'state_dict' in checkpoint:
@@ -43,18 +79,18 @@ def test(config, model_path, output_dir, logger=None):
     model.eval()
     all_preds = []
     all_ids = []
-    
     with torch.no_grad():
-        for inputs, ids in tqdm(test_loader, desc="Generating predictions"):
-            inputs = inputs.to(device)
-            outputs = model(inputs)
-            _, predicted = outputs.max(1)
-            
+        for images, ids in test_loader:
+            images = images.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
             all_preds.extend(predicted.cpu().numpy().tolist())
             all_ids.extend(ids.tolist())
+
+    # Create submission CSV (must have exactly two columns: "ID" and "Labels")
     submission_df = pd.DataFrame({
-        'ID': all_ids,
-        'Labels': all_preds
+        "ID": all_ids,
+        "Labels": all_preds
     })
     submission_path = f"{output_dir}/submission.csv"
     submission_df.to_csv(submission_path, index=False)
